424f417d38995aac6ef6e2f99c03091d7c8bf8d7
hanishvashi <hanishvashi@gmail.com>
2023-08-18 20:53:00 +0530

    Update payment-india.blade.php
    disabled cod

--- project/resources/views/load/payment-india.blade.php
+++ project/resources/views/load/payment-india.blade.php
@@ -9,10 +9,11 @@
         <div class="icon"><span class="radio"></span></div>
         <p>PayuMoney <small></small></p>
         </a>
+        {{--
         <a class="nav-link payment" data-val="" data-show="no" data-form="{{route('cash.submit')}}" data-href="{{ route('front.load.payment',['slug1' => 'cod','slug2' => 0]) }}" id="v-pills-tab2-tab" data-toggle="pill" href="#v-pills-tab2" role="tab" aria-controls="v-pills-tab2" aria-selected="true">
         <div class="icon"><span class="radio"></span></div>
         <p>Cash on Delivery <small></small></p>
-        </a>
+        </a> --}}
           @else
           <a class="nav-link payment" data-val="" data-show="no" data-form="{{route('payu.submit')}}" data-href="{{ route('front.load.payment',['slug1' => 'payu','slug2' => 0]) }}" id="v-pills-tab1-tab" data-toggle="pill" href="#v-pills-tab1" role="tab" aria-controls="v-pills-tab1" aria-selected="true">
           <div class="icon"><span class="radio"></span></div>
